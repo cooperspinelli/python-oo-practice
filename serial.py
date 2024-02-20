46749147916f7f59dbd4382a849b819d3a5bc6cc
Cooper Spinelli <cooperspinelli@gmail.com>
2024-02-20 15:59:29 -0700

    Implement serializer

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,19 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """Initializes instance variables start and value"""
+        self.start = self.next = start
+
+    def __repr__(self):
+        return f"Start value is {self.start}. Current value is {self.next}"
+
+    def generate(self):
+        """Increments instance variable value and then returns it"""
+        self.next += 1
+        return self.next - 1
+
+    def reset(self):
+        """Resets instance variable value to the starting value"""
+        self.next = self.start
