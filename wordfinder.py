ad0c4c9040fbb1d163a4532ee78aa2c94f90bba2
Cooper Spinelli <cooperspinelli@gmail.com>
2024-02-20 17:33:47 -0700

    Refactored and added test files

--- wordfinder.py
+++ wordfinder.py
@@ -5,36 +5,42 @@ class WordFinder:
     """Word Finder: finds random words from a dictionary."""
 
     def __init__(self, path):
-        """Creates attribute words and assigns it to be a list of words
-        in the file found at the input path
-        Also creates attribute path which saves the input path"""
+        """Initializes attribute words to be an empty list,
+        creates attribute path which saves the input path,
+        and calls read_file which populates words attribute with lines
+        from the file found at path"""
 
         self.path = path
-        self.words = self.__read_file__(path)
+        self.words = []
+        self.__read_file__(path)
 
         print(f"{len(self.words)} words read")
 
     def __repr__(self):
         """Returns representation of instance of WordFinder class"""
+
         return f"WordFinder created from {self.path}"
 
+    def __append_line__(self, line):
+        """Strips trailing whitespace off of line and
+        appends it to words attribute"""
+
+        self.words.append(line.rstrip())
+
     def __read_file__(self, path):
-        """Takes in file path and returns list of words in that file
+        """Takes in file path and calls append_line on each line in that file
         If file does not exist then it prints 'File does not exist' and
         returns an empty list"""
-
-        words = []
 
         try:
             file = open(path)
             for line in file:
-                words.append(line.rstrip())
+                self.__append_line__(line)
             file.close()
 
         except FileNotFoundError:
             print('File does not exist')
 
-        return words
 
     def random(self):
         """Returns a random word from self.words"""
@@ -45,22 +51,9 @@ class RandomWordFinder(WordFinder):
     """Sub-class of WordFinder. Removes lines that start with '#' and empty
     lines."""
 
-    def __read_file__(self, path):
-        """Takes in file path and returns list of words in that file
-        If file does not exist then it prints 'File does not exist' and
-        returns an empty list"""
+    def __append_line__(self, line):
+        """Checks if line is valid, if it is, strips trailing whitespace
+        from line and appends it to words attribute"""
 
-        words = []
-        invalid_characters = ['#', '\n']
-
-        try:
-            file = open(path)
-            for line in file:
-                if line[0] not in invalid_characters:
-                    words.append(line.rstrip())
-            file.close()
-
-        except FileNotFoundError:
-            print('File does not exist')
-
-        return words
+        if line[0] not in ['#', '\n']:
+            self.words.append(line.rstrip())
